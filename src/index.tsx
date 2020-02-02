cae818a1e10fd124794067c57f143dabd3c86f3d
giancarlo <giancarlo@messagebird.com>
2020-02-02 12:12:15 +0100

    chore: initial project setup

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return <div>hallo</div>
+}
+
+
+ReactDOM.render(<App />, document.getElementById('chuck-norris-app'));
